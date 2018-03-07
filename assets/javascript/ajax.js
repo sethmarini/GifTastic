8f20a0aa36ad01042429fb29b2fbbfa31075fb5f
sethmarini <33646237+sethmarini@users.noreply.github.com>
2018-03-07 14:23:30 -0500

    update

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-var topics = ['lions','tigers','bears','sharks'];
+	var topics = ['lions','tigers','bears','sharks'];
 
 
 
@@ -20,13 +20,14 @@ var createButton = function() {
 	}
 };
 
-	createButton();
+createButton();
 
 // Adding buttons
 
 $("#searchButton").on("click", function() {
 
 	event.preventDefault();
+
 	var input = $("#inputId").val().trim();
 
 	topics.push(input);
@@ -39,7 +40,7 @@ $("#searchButton").on("click", function() {
 // Button Functionality
 
 $(document).on("click", ".animal", function() {
-	
+
 	var searchTerm = $(this).html();
 	
 	var key = "2orTtjnU84tc7bM0NzkX1DE8B0UfWJKg";
@@ -47,18 +48,33 @@ $(document).on("click", ".animal", function() {
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + key +"&limit=10";
 
 	$.ajax({
+
 		url: queryURL,
+
 		method: "GET"
+
 	}).then(function(results) {
+
+
+		$("#gifShow").empty();
 
 		for(var i = 0; i < results.data.length; i++) {
 			var rating = results.data[i].rating;
 			var url = results.data[i].images.fixed_height_small.url;
+			var urlStill = results.data[i].images.fixed_height_small_still.url;
+
 
 			var newDiv = $("<div>");
 			var newImg = $("<img>");
 			
-			newImg.attr("src", url);
+
+			newImg.attr("src", urlStill);
+			newImg.attr("gif-still", urlStill);
+			newImg.attr("gif-active", url);
+			newImg.attr("gif-choice", "still");
+			newImg.addClass(".gifClick");
+			
+
 
 			var newP = $("<p>");
 
@@ -72,8 +88,27 @@ $(document).on("click", ".animal", function() {
 		console.log(results);
 	});
 
+
+// image status
+	$(document).on("click", ".gifClick", function() {
+
+		var status = $(this).attr("gif-choice");
+
+		if (status == "still" ) {
+			$(this).attr("src", $(this).attr("gif-active"));
+			$(this).attr("gif-choice", "active");
+		}
+
+		else if (status == "active") {
+			$(this).attr("src", $(this).attr("gif-still"));
+			$(this).attr("gif-choice", "still");
+
+
+		}
+
+	});
+
+
 });
-
-
 
 });
